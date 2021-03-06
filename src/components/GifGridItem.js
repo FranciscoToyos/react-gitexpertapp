dcb0b6c48448c4c9803e97afa4b7d339c2c57247
FranciscoToyos <Francabj95@gmail.com>
2021-03-06 11:55:30 -0300

    Bug solucionado

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-export const GifGridItem = ({id,title,url}) => {
+export const GifGridItem = ({ title, url }) => {
 
-    console.log(id,title,url);
     return (
-        <div className='card animate__animated animate__fadeInDown'>
-            <img src={url} alt={title}/>
-            <p>{title}</p>
+        <div className="card animate__animated animate__fadeIn">
+            <img src={ url } alt={ title } />
+            <p> { title } </p>
         </div>
     )
 }
